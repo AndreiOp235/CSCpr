136e5da7151d469139d76eb1bace0c70f7743730
Coșa Andrei Valeriu <42703353+AndreiOp235@users.noreply.github.com>
2025-02-18 22:23:19 +0200

    config MS

--- MS-JT.c
+++ MS-JT.c
@@ -74,40 +74,7 @@ void main (void) {
 				break;
 #endif
 
-#if(PROTOCOL == JT)	// nodul nu detine jetonul, asteapta un mesaj util sau jetonul
-				
-				switch(RxMesaj(ADR_NOD)){					// asteapta jetonul de la master
-					case TMO:
-																					// anunta ca nodul a regenerat jetonul 
-																					// nodul curent detine jetonul
-																					// Daca e permisa afisarea, afiseaza meniul de comenzi
-																					// trece in starea 1
-						break;
-
-					case ROK:										break;						// a primit un mesaj USER_MES, il afiseaza
-					case JOK:										// a primit un jetonul
-						
-						Delay(WAIT/2);						// asteapta WAIT/2 ms
-					
-																						// adresa HW dest este adr_hw_src
-																						// adresa HW src este ADR_NOD
-																						// tip mesaj = JET_MES
-																						// transmite mesajul JET_MES din bufferul ADR_NOD
-																						// nodul curent detine jetonul
-						if(AFISARE) 										// Daca e permisa afisarea, afiseaza meniul de comenzi		
-																						// trece in starea 1
-						break; // nodul detine jetonul, poate trece sa transmita un mesaj de date
-
-					case CAN:												break;		// afiseaza eroare Mesaj incomplet
-					case CAN_adresa_hw_src:	 				break;		// afiseaza eroare Mesaj incomplet (adresa_hw_src)
-					case CAN_tipmes:								break;		// afiseaza eroare Mesaj incomplet (tip mesaj)
-					case CAN_sc:										break;		// afiseaza eroare Mesaj incomplet (sc)
-					case TIP:												break;		// afiseaza eroare Tip mesaj necunoscut
-					case ESC:	 											break;				// afiseaza Eroare SC
-					default:												break;			// afiseaza cod eroare necunoscut, apoi asteapta 1 sec
-				}
-				break;
-#endif								
+			
 
 			case 1:											
 
@@ -132,24 +99,7 @@ void main (void) {
 																// trece in starea 0, sa astepte un nou mesaj de la master
 				break;
 #endif
-
-#if(PROTOCOL == JT)							// nodul detine jetonul, poate transmite un mesaj USER_MES				
-				
-				
-																			// cauta sa gaseasca un mesaj util de transmis
 	
-
-
-
-
-																					// daca gaseste un mesaj de transmis catre nodul i
-																						// adresa HW dest este dest
-																						// transmite mesajul catre nodul i
-																						// asteapta procesarea mesajului la destinatie (WAIT/2 msec)
-
-																		// va incerca sa transmita jetonul nodului urmator 
-																		// trece in starea 2, sa transmita jetonul urmatorului nod
-#endif	
 				
 			break;	
 				
@@ -173,21 +123,6 @@ void main (void) {
 
 #endif
 
-#if(PROTOCOL == JT)											// nodul transmite jetonul urmatorului nod 		
-				
-																				// selecteaza urmatorul slave (incrementeaza i)
-					
-	
-	
-	
-				Delay(WAIT/2);									// asteapta WAIT/2 sec
-				
-																				// adresa HW dest este i
-																				// adresa HW src este ADR_NOD
-																				// tip mesaj = JET_MES
-																				// transmite mesajul din bufferul i
-																				// trece in starea 3, sa astepte confirmarea de la nodul i ca jetonul a fost primit
-#endif
 
 			break;
 
@@ -218,55 +153,7 @@ void main (void) {
 #endif
 
 			
-#if(PROTOCOL == JT)					
-				switch(RxMesaj(ADR_NOD)){									// asteapta un raspuns de la nod i
-					case TMO:	Error("\n\rTimeout nod ");		// afiseaza eroare Timeout nod i
-								
 
-																									// revine in starea 2 (nu a primit raspuns)
-								break;
-					case JOK:																// a primit confirmarea transferului jetonului, revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// daca afisarea e permisa, afiseaza meniul
-								break;
-					case ERI:																// afiseaza Eroare incadrare
-																									// revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// afiseaza meniul
-										break;			
-					case ERA:																// afiseaza Eroare adresa
-																									// revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// afiseaza meniul
-										break;			
-					
-					case CAN:																// afiseaza Mesaj incomplet
-																									// revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// afiseaza meniul
-										break;			
-					
-					case TIP:																// afiseaza Tip mesaj necunoscut
-																									// revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// afiseaza meniul
-										break;			
-					
-					case ESC:																// afiseaza Eroare SC
-																									// revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// afiseaza meniul
-										break;			
-
-					default:																// afiseaza Eroare necunoscuta
-																									// asteapta 1000 ms
-																									// revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// afiseaza meniul
-										break;			
-			}
-				
-#endif
 			
 			break;			
 		}
